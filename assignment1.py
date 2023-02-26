fa45aca71e86879bbcdcfd459689b9ab6de3ff78
michalea shofner <61432207+mgshofner@users.noreply.github.com>
2023-02-26 18:57:33 -0500

    assignment 2/26

--- assignment1.py
+++ assignment1.py
@@ -1,29 +1,69 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import datetime
+import time
 
 data = pd.read_csv("pricing.csv")
-#input_shape = (5,)
+data = data[:-1]
+data = data.sample(n = 5000)
 
-# Define the model
-inputs = tf.keras.layers.Input(shape=(5,), name='input')
-hidden1 = tf.keras.layers.Dense(units=5, activation="sigmoid", name = 'hidden1')(inputs)
-hidden2 = tf.keras.layers.Dense(units=5, activation="sigmoid", name= 'hidden2')(hidden1)
-hidden3 = tf.keras.layers.Dense(units=5, activation="sigmoid", name= 'hidden3')(hidden2)
-output = tf.keras.layers.Dense(units=1, activation = "linear", name= 'output')(hidden3)
+dprice = data['price'].max() - data['price'].min()
+dorder = data['order'].max() - data['order'].min()
+dduration = data['duration'].max() - data['duration'].min()
+dquantity = data['quantity'].max() - data['quantity'].min()
 
-model = tf.keras.Model(inputs = inputs, outputs = output)
+#dummies for category
+# data = pd.get_dummies(data, columns=['category'])
+# data['category'] = v4.to_numpy() 
 
-model.compile(loss = 'mse', optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001))
+#normalize the variables
+diffs = {'price': dprice, 'order': dorder, 'duration': dduration, 'quantity': dquantity}
+for col, diffs in diffs.items():
+    data[col] = data[col]/diffs
+data.head()
 
-#Iterate over all rows in the dataframe to incrementally train the model
+#model architecture
+inputs = tf.keras.layers.Input(shape=(3,), name='input')
+hidden1 = tf.keras.layers.Dense(units=5, activation="sigmoid", name='hidden1')(inputs)
+hidden2 = tf.keras.layers.Dense(units=3, activation="sigmoid", name='hidden2')(hidden1)
+hidden3 = tf.keras.layers.Dense(units=2, activation="sigmoid", name='hidden3')(hidden2)
+output = tf.keras.layers.Dense(units=1, activation="linear", name='output')(hidden3)
+model = tf.keras.Model(inputs=inputs, outputs=output)
+
+# Compile the model
+
+model.compile(loss='mse', optimizer=tf.keras.optimizers.SGD(learning_rate=0.01))
+
+time_start = datetime.datetime.now()
+loss_history = []
+#read in rows 1 at a time
 for i, row in data.iterrows():
-    sku = row['sku']
-    price = row['price']
-    order = row['order']
-    duration = row['duration']
-    category = row['category']
-    quantity = row['quantity']
-    X = [np.column_stack([[sku, price, order, duration, category]])]
-    y = [quantity]
-    model.train_on_batch(X, y)
+    X = np.array(row[['price', 'order', 'duration']]).reshape(1,3)
+    y = np.array(row['quantity']).reshape(1, 1)
+    history = model.fit(x = X, y = y, batch_size = 1, epochs = 2)
+    loss_history.append(history.history['loss'])
+print(datetime.datetime.now() - time_start)
+
+plt.plot(loss_history)
+plt.show()
+
+test = pd.read_csv("pricing_test.csv")
+test.columns =['sku', 'price', 'quantity', 'order', 'duration', 'category']
+
+diffs = {'price': dprice, 'order': dorder, 'duration': dduration, 'quantity': dquantity}
+for col, diffs in diffs.items():
+    test[col] = test[col]/diffs
+test.head()
+
+X_test = test[['price', 'order', 'duration']].values
+y_test = test['quantity'].values
+yhat = model.predict(x=X_test)
+yhat
+model.evaluate(X,y)
+model.summary()
+correlation = np.corrcoef(yhat.flatten(), y_test)[0, 1]
+correlation
+plt.plot(yhat)
+plt.show()
